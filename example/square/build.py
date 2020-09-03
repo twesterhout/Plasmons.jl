6863888846b9e9e997d813a3126aae864bf5a0bc
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-09-03 10:20:44 +0200

    Generate proper input hdf5 file in Python

--- example/square/build.py
+++ example/square/build.py
@@ -1,4 +1,7 @@
 import tipsi
+import numpy as np
+import scipy.sparse
+import h5py
 
 
 def square(width: int, a: float, t: float) -> tipsi.builder.Sample:
@@ -20,10 +23,21 @@ def square(width: int, a: float, t: float) -> tipsi.builder.Sample:
     return sample
 
 
+def get_hamiltonian(sample: tipsi.Sample) -> np.ndarray:
+    n = len(sample.site_x) # number of sites
+    sparse = scipy.sparse.csr_matrix((sample.hop, sample.indices, sample.indptr), shape=(n, n))
+    return sparse.todense()
+
+
+def get_coulomb(sample: tipsi.Sample) -> np.ndarray:
+    pass
+
+
 def main():
-    n = 35
+    n = 10
     sample = square(n, 1.0, 1.0)
-    sample.save("square_sheet_{}x{}.hdf5".format(n, n))
+    with h5py.File("square_sheet_{}x{}.h5".format(n, n), "w") as f:
+        f.create_dataset("H", data=get_hamiltonian(sample))
 
 
 if __name__ == "__main__":

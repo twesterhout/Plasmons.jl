8def49e3d1733cb57e613a0649532ff5ddcb2715
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-09-08 23:23:27 +0200

    Example runner script

---
+++ example/square/run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export OMP_NUM_THREADS=4
+exec julia -e 'import Plasmons; Plasmons.entry_main()' -- \
+	--kT 0.025 --mu 0.4 --damping 0.006 \
+	--hamiltonian H --coulomb V \
+	--frequency "$(seq -s, 0.0 0.05 15.0)" \
+	square_sheet_10x10.h5 result_10x10.h5

528620855025caa47fafb8f0e934589483b9fa65
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-09-13 16:20:08 +0200

    Remove precompile scripts

--- scripts/build.jl
+++
@@ -1,8 +0,0 @@
-using PackageCompiler
-
-create_app(
-    joinpath(dirname(@__FILE__), ".."),
-    "plasmons-v1.0.0",
-    precompile_statements_file = joinpath(dirname(@__FILE__), "..", "precompile_file.jl"),
-    filter_stdlibs = true,
-)

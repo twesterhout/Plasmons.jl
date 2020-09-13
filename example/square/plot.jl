e5edd6081d771c04df8cfe7d69aeec9839fde80d
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2020-09-13 16:19:22 +0200

    Update example for square lattice

---
+++ example/square/plot.jl
@@ -0,0 +1,51 @@
+using Plots
+using LaTeXStrings
+using LinearAlgebra
+using HDF5
+using Plasmons
+gr()
+
+theme(
+    :solarized_light;
+    fg = RGB(88 / 255, 110 / 255, 117 / 255),
+    fgtext = RGB(7 / 255, 54 / 255, 66 / 255),
+    fgguide = RGB(7 / 255, 54 / 255, 66 / 255),
+    fglegend = RGB(7 / 255, 54 / 255, 66 / 255),
+)
+
+function plot_sample(x::AbstractVector, y::AbstractVector)
+    scatter(
+        x,
+        y,
+        label = "",
+        size = (400, 400),
+        aspect_ratio = :equal,
+        grid = false,
+        showaxis = false,
+        ticks = nothing,
+    )
+end
+plot_sample(file::HDF5File) = plot_sample(read(file, "x"), read(file, "y"))
+plot_sample(file::AbstractString) =
+    h5open(io -> plot_sample(read(io, "x"), read(io, "y")), file, "r")
+
+function plot_dos(E::AbstractVector)
+    histogram(E, bins = 30, xlabel = "E", ylabel = "ρ", normalize = true, label = nothing)
+end
+plot_dos(H::AbstractMatrix) = plot_dos(eigvals(Hermitian(H)))
+plot_dos(file::HDF5File) = plot_sample(read(file, "H"))
+
+function make_dispersion(ε, ks, x, y, z)
+    map(k -> Plasmons.dispersion((k, 0.0, 0.0), ε, x, y, z), ks)
+end
+function make_dispersion(file::HDF5File, x, y, z)
+    a = minimum(filter(_x -> _x > 0, x))
+    ks = (-π / a):(π / a / 100):(π / a)
+    ωs = map(d -> real(read(attrs(d), "ħω")), file["ε"])
+    out = Array{ComplexF64}(undef, length(ωs), length(ks))
+    for (i, ε) in enumerate(map(read, file["ε"]))
+        out[i, :] = make_dispersion(ε, ks, x, y, z)
+    end
+    out = -1 ./ out
+    heatmap(ks, ωs, imag(out))
+end
--- example/square/run.sh
+++ example/square/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export OMP_NUM_THREADS=4
-exec julia -e 'import Plasmons; Plasmons.entry_main()' -- \
+exec julia -e 'import Plasmons; Plasmons.julia_main()' -- \
 	--kT 0.025 --mu 0.4 --damping 0.006 \
 	--hamiltonian H --coulomb V \
 	--frequency "$(seq -s, 0.0 0.05 15.0)" \

6c832ada7d5ec875d9e0d6d9b78425132519f1b6
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-02-19 19:57:35 +0100

    A pretty picture

--- example/square/plot.jl
+++ example/square/plot.jl
@@ -3,9 +3,10 @@ using LaTeXStrings
 using LinearAlgebra
 using HDF5
 using DelimitedFiles
-# using Plasmons
-gr()
+using LsqFit
+using Printf
 
+pgfplotsx()
 theme(
     :solarized_light;
     fg = RGB(88 / 255, 110 / 255, 117 / 255),
@@ -18,7 +19,8 @@ function extract_time(file::HDF5.File, i::Int; group = "/χ")
     i = string(i, pad = 4)
     read(attributes(file["$group/$i"]), "time")
 end
-extract_time(file::AbstractString, i::Int; kwargs...) = h5open(io -> extract_time(io, i; kwargs...), file, "r")
+extract_time(file::AbstractString, i::Int; kwargs...) =
+    h5open(io -> extract_time(io, i; kwargs...), file, "r")
 
 function extract_time(sizes::AbstractVector{Int}; suffix)
     map(n -> extract_time("output_square_$(n)x$(n)_$suffix.h5", 2), sizes)
@@ -26,60 +28,49 @@ end
 
 function save_timings(sizes)
     sizes = collect(sizes)
-    cpu = extract_time(sizes; suffix="cpu")
-    cuda = extract_time(sizes; suffix="cuda")
+    cpu = extract_time(sizes; suffix = "cpu")
+    cuda = extract_time(sizes; suffix = "cuda")
     writedlm("benchmark_result_square.dat", [sizes cpu cuda])
 end
 
-function plot_times()
-    sizes = collect(5:5:40)
-    cpu = extract_time(sizes; suffix="cpu")
-    cuda = extract_time(sizes; suffix="cuda")
-    g = plot(sizes, cpu, label = "Xeon E5-2450 v2")
-    plot!(g, sizes, cuda, label = "Tesla K40")
-    savefig(g, "")
+function determine_scaling(x, y)
+    @. model(x, p) = p[1] + p[2] * x
+    p0 = [-10.0, 4.0]
+    fit = curve_fit(model, log.(x), log.(y), p0)
+
+    f = n -> exp(fit.param[1]) * n .^ fit.param[2]
+    return fit.param[2], f
 end
 
-# function plot_sample(x::AbstractVector, y::AbstractVector)
-#     scatter(
-#         x,
-#         y,
-#         label = "",
-#         size = (400, 400),
-#         aspect_ratio = :equal,
-#         grid = false,
-#         showaxis = false,
-#         ticks = nothing,
-#     )
-# end
-# plot_sample(file::HDF5File) = plot_sample(read(file, "x"), read(file, "y"))
-# plot_sample(file::AbstractString) =
-#     h5open(io -> plot_sample(read(io, "x"), read(io, "y")), file, "r")
+function plot_times()
+    table = readdlm("benchmark_result_square.dat")
+    sizes, cpu, cuda = table[:, 1], table[:, 2], table[:, 3]
+    sizes = sizes .^ 2 # benchmark.sh stores width of the sample rather
+    cpu_exp, cpu_line = determine_scaling(sizes[5:end], cpu[5:end])
+    cuda_exp, cuda_line = determine_scaling(sizes[5:end], cuda[5:end])
+    continuous_size = collect(sizes[begin]:0.1:sizes[end])
 
-# function plot_dos(E::AbstractVector)
-#     histogram(E, bins = 30, xlabel = "E", ylabel = "ρ", normalize = true, label = nothing)
-# end
-# plot_dos(H::AbstractMatrix) = plot_dos(eigvals(Hermitian(H)))
-# plot_dos(file::HDF5File) = plot_sample(read(file, "H"))
-
-# function make_dispersion(ε, ks, x, y, z)
-#     map(k -> Plasmons.dispersion((k, 0.0, 0.0), ε, x, y, z), ks)
-# end
-# function make_dispersion(file::HDF5File, x, y, z)
-#     a = minimum(filter(_x -> _x > 0, x))
-#     ks = (-π / a):(π / a / 100):(π / a)
-#     ωs = map(d -> real(read(attrs(d), "ħω")), file["ε"])
-#     out = Array{ComplexF64}(undef, length(ωs), length(ks))
-#     for (i, ε) in enumerate(map(read, file["ε"]))
-#         out[i, :] = make_dispersion(ε, ks, x, y, z)
-#     end
-#     out = -1 ./ out
-#     heatmap(ks, ωs, imag(out))
-# end
-# function make_dispersion(file::HDF5File, x, y, z)
-#     # ωs = map(d -> real(read(attrs(d), "ħω")), file["ε"])
-#     out = dispersion(map(read, file["ε"]), (1.0, 0.0, 0.0), x, y, z; n = 1000)
-#     # @info out
-#     out = imag(-1 ./ out)
-#     heatmap(out)
-# end
+    g = plot(xlabel = "System size", ylabel = "Time per frequency, seconds", legend = :topleft)
+    plot!(
+        g,
+        continuous_size,
+        cpu_line(continuous_size),
+        linewidth = 2,
+        linecolor = :black,
+        linealpha = 0.6,
+        label = nothing,
+    )
+    plot!(
+        g,
+        continuous_size,
+        cuda_line(continuous_size),
+        linewidth = 2,
+        linecolor = :black,
+        linealpha = 0.6,
+        label = nothing,
+    )
+    scatter!(g, sizes, cpu, label = @sprintf("CPU (Xeon E5-2450 v2), ν = %.2f", cpu_exp))
+    scatter!(g, sizes, cuda, label = @sprintf("GPU (Tesla K40), ν = %.2f", cuda_exp))
+    savefig(g, "benchmark_result_square.png")
+    g
+end

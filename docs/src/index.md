db53b906808b5606eaad5b3cc8fafb5b99a3de68
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-02-19 11:07:20 +0100

    Updating documentation

--- docs/src/index.md
+++ docs/src/index.md
@@ -1,18 +1,73 @@
 # Plasmons.jl
 
+Within *real-space* RPA (Random Phase Approximation) the calculation of
+polarizability matrix ``\chi(\omega)`` amounts to evaluation of the following
+equation
+```math
+\begin{aligned}
+    \chi_{a,b}(\omega)
+        &= \langle a |\hat\chi(\omega)| b \rangle
+        = 2\cdot \sum_{i,j} \langle i |\hat G(\omega)| j \rangle
+           \langle j | a \rangle \langle a | i \rangle
+           \langle i | b \rangle \langle b | j \rangle\; , \\
+    G_{i,j}(\omega)
+        &= \langle i | \hat G(\omega) | j \rangle
+        = \frac{f_i - f_j}{E_i - E_j - (\hbar\omega + i\eta)} \;.
+\end{aligned}
+```
+
+Here ``E_i`` is the ``i``'th energy eigenvalue, ``\langle a | i \rangle = \psi_i
+(a)`` -- the ``i``'th eigenstate, ``f_i = f(E_i)`` -- occupation of the ``i``'th
+state given by the Fermi-Dirac distribution, ``\eta`` is the Landau damping, and
+``\omega`` is the frequency.
+
+Then, if you have access to *bare* (also called *unscreened*) Coulomb interaction
+``V``, dielectric function ``\varepsilon(\omega)`` can be computed as follows:
+
+```math
+    \varepsilon(\omega) = 1 - \chi(\omega) \cdot V \;.
+```
+
+And with ``\varepsilon(\omega)`` you essentially have full optical description
+of your system and can do all kind of interesting physics.
+
+
+!!! note
+
+    This package allows you to compute ``\chi(\omega)`` and ``\varepsilon(\omega)``
+    as efficiently as possible. Think of it as
+    [BLAS](https://en.wikipedia.org/wiki/Basic_Linear_Algebra_Subprograms) for
+    real-space RPA: writing matrix-matrix multiplication function is trivial and
+    everybody can do it, but good luck making it competitive with OpenBLAS or Intel
+    MKL! Same thing here: calculation of ``\chi(\omega)`` can be done in 10 lines of
+    code, but boy will it be slow! 😝
+
+
 ## Installing
 
-Plasmons.jl can be used either as a Julia package or as a standalone executable.
-The latter option is provided for people who do not care about Julia and just
-want to do physics. If you are one of them just download `Plasmons.sif`
-executable file from the
-[Releases](https://github.com/twesterhout/Plasmons.jl/releases) page and you are
-good to go.
+[`Plasmons.jl`](https://github.com/twesterhout/Plasmons.jl) can be used either
+as a Julia package or as a [Singularity](https://sylabs.io/) container.
 
-Otherwise, install Plasmons.jl using Pkg:
+#### Julia package
+
+Install using Pkg:
+
 ```julia
 import Pkg; Pkg.add(url="https://github.com/twesterhout/Plasmons.jl")
 ```
+
+
+#### Singularity container
+
+This option is provided for people who do not care about Julia and just want to
+do physics.
+
+Download `Plasmons.sif` file from the
+[Releases](https://github.com/twesterhout/Plasmons.jl/releases/latest) page and
+you are good to go.
+
+
+## Example: square lattice
 
 
 ## Using the executable
@@ -40,65 +95,10 @@ system. If you additionally provide unscreened Coulomb interaction ``V``,
 
 
 
-## Algorithm
-
-Within real-space RPA (Random Phase Approximation) the calculation of
-polarizability matrix ``\chi(\omega)`` amounts to evaluation of the following equation
-```math
-\begin{aligned}
-    \chi_{a,b}
-        &= \langle a |\hat\chi| b \rangle
-        = 2\cdot \sum_{i,j} \langle i |\hat G| j \rangle
-           \langle j | a \rangle \langle a | i \rangle
-           \langle i | b \rangle \langle b | j \rangle\; , \\
-    G_{i,j}
-        &= \langle i | \hat G | j \rangle
-        = \frac{f_i - f_j}{E_i - E_j - (\hbar\omega + i\eta)} \;.
-\end{aligned}
-```
-
-Here ``E_i``s are energy eigenvalues, ``\langle a | i \rangle = \psi_i (a)`` are
-energy eigenstates, ``f_i = f(E_i)`` is the occupation of the ``i``'th state
-given by the Fermi-Dirac distribution, ``\eta`` is Landau damping, and
-``\omega`` is the frequency.
-
-First of all, for a given temperature ``T`` and chemical potential ``\mu``, we
-compute occupation numbers ``f(E_i)``.
+## Reference
 
 ```@docs
-    Plasmons.fermidirac
+    Plasmons.polarizability
+    Plasmons.dielectric
+    Plasmons.dispersion
 ```
-
-Next, we compute the matrix ``G``. This is done by either [`Plasmons._g`](@ref)
-or [`Plasmons._g_blocks`](@ref) functions.
-
-```@docs
-    Plasmons._g
-    Plasmons._g_blocks
-```
-
-Let us now turn to the computation of polarizability matrix ``\chi``. The naive
-approach is to write 4 nested loops. However, this is tremendously slow! A
-slightly better approach which I used for my Bachelor thesis is to rewrite
-the computation of each ``\chi_{a, b}`` as a combination of `GEMV` and `DOT`
-operations:
-
-```math
-\begin{aligned}
-    \chi_{a, b}
-        &= \sum_{i, j} \left(\langle a | i \rangle \langle i | b \rangle\right)
-            G_{i, j}
-            \left(\langle j | a \rangle \langle b | j \rangle\right)
-         = A(a,b)^\dagger G A(a,b) \;, \\
-        &\text{where } A(a,b)_j = \langle j | a \rangle \langle b | j \rangle \;.
-\end{aligned}
-```
-
-We use the following data structure to hold ``A`` as well as the temporary ``G
-A``.
-
-```@docs
-    Plasmons._Workspace
-```
-
-**TODO**: Finish this.
--- src/Plasmons.jl
+++ src/Plasmons.jl
@@ -1,7 +1,7 @@
 module Plasmons
 
-export dielectric, polarizability
-export julia_main
+export dielectric, polarizability, dispersion
+export main, julia_main
 
 using CUDA
 using Adapt
@@ -25,9 +25,7 @@ physical dimension of `kT` is the same as `E` (e.g. electron-volts).
 @inline fermidirac(E; mu, kT) = 1 / (1 + exp((E - mu) / kT))
 
 function _build_g(ħω::Complex{ℝ}, E::AbstractVector{ℝ}; mu::ℝ, kT::ℝ) where {ℝ <: Real}
-    # Compared to a simple `map` the following saves one allocation
-    f = similar(E)
-    map!(x -> fermidirac(x, mu = mu, kT = kT), f, E)
+    f = map(x -> fermidirac(x, mu = mu, kT = kT), E)
     n = length(E)
     G = similar(E, complex(ℝ), n, n)
     @inbounds for j in 1:n
@@ -44,6 +42,12 @@ function _postprocess_g(
     compress_if(x) = blocks ? ThreeBlockMatrix(x) : x
     return _map_tuple(compress_if, split_if(G))
 end
+"""
+    _map_tuple(f::Function, x)
+
+Apply `f` to every element of `x`, but only if `x` is a `Tuple`. If not, just apply `f` to
+the whole `x`.
+"""
 _map_tuple(f::Function, x::Tuple) = f.(x)
 _map_tuple(f::Function, x::Any) = f(x)
 
@@ -70,7 +74,7 @@ and a single object is returned.
 Sometimes one can further exploit the structure of ``G``. For ``E \ll \mu`` or ``E \gg \mu``
 Fermi-Dirac distribution is just a constant and ``G`` goes to 0 for all ``\omega``. We can
 use this fact to construct a block-sparse version of ``G`` to speed up matrix-matrix
-products containing it. To return ``G`` in block-sparse form, set `block = true`.
+products containing it. To return ``G`` in block-sparse form, set `blocks = true`.
 
 This function works with both CPU and GPU arrays. If vector `E` is stored on the GPU, then
 the returned matrix (or all submatrices for block-sparse version) will also reside on GPU.
@@ -90,15 +94,18 @@ g_matrix(
 )
 
 @doc raw"""
-    polarizability(ħω, E, ψ; mu, kT, method = :batched) -> χ
+    polarizability(ħω::ℂ, E, ψ; mu::ℝ, kT::ℝ, method = :batched, blocks = true) -> χ
 
 Compute polarizability matrix ``\chi`` using method `method` (either `:simple` or
-`:batched`).
+`:batched`). `ħω` specifies complex frequency including Landau damping (i.e. ``\hbar\omega +
+i\eta``). `mu` and `kT` specify chemical potential and temperature respectively. `E` and `ψ`
+are eigenvalues and eigenvectors of the system Hamiltonian. They can be either CPU or GPU
+arrays.
 """
 function polarizability(
     ħω::Complex,
-    E::AbstractVector,
-    ψ::AbstractMatrix;
+    E::AbstractVector{<:Real},
+    ψ::AbstractMatrix{<:Union{Real, Complex}};
     mu::Real,
     kT::Real,
     method::Symbol = :batched,
@@ -123,6 +130,11 @@ function polarizability(
 end
 
 
+struct Workspace{M <: AbstractMatrix}
+    A::M
+    temp::M
+end
+
 @doc raw"""
     polarizability_batched(ħω::ℂ, E, ψ; mu::ℝ, kT::ℝ, blocks::Bool) -> χ
 
@@ -132,17 +144,12 @@ function polarizability_batched(
     ħω::Complex,
     E::AbstractVector{ℝ},
     ψ::AbstractMatrix{ℂ};
-    mu::Real,
-    kT::Real,
+    mu::ℝ,
+    kT::ℝ,
     blocks::Bool,
 ) where {ℝ <: Real, ℂ <: Union{ℝ, Complex{ℝ}}}
     G = g_matrix(ℂ, ħω, E; mu = mu, kT = kT, blocks = blocks)
     ℂ <: Real ? polarizability_batched(G[1], G[2], ψ) : polarizability_batched(G, ψ)
-end
-
-struct Workspace{M <: AbstractMatrix}
-    A::M
-    temp::M
 end
 function polarizability_batched(
     Gᵣ::Union{AbstractMatrix{ℝ}, ThreeBlockMatrix{ℝ}},
@@ -177,7 +184,7 @@ function polarizability_batched(
 end
 function _batched_mat_el!(
     out::AbstractVector{Complex{ℝ}},
-    ws,
+    ws::Workspace,
     as::UnitRange{Int},
     b::Int,
     Gᵣ::Union{AbstractMatrix{ℝ}, ThreeBlockMatrix{ℝ}},
@@ -188,7 +195,6 @@ function _batched_mat_el!(
     A = view(ws.A, 1:batch_size, :)
     temp = view(ws.temp, 1:batch_size, :)
     A .= view(ψ, as, :) .* transpose(view(ψ, b, :))
-    # @info "" typeof(temp) typeof(A) typeof(Gᵣ)
     _mul_with_strides!(temp, A, Gᵣ, one(ℝ), zero(ℝ))
     outᵣ = view(reinterpret(ℝ, out), 1:2:(2 * batch_size))
     dot_batched!(outᵣ, A, temp)
@@ -199,7 +205,7 @@ function _batched_mat_el!(
 end
 function _batched_mat_el!(
     out::AbstractVector{ℂ},
-    ws,
+    ws::Workspace,
     b::Int,
     G::Union{AbstractMatrix{ℂ}, ThreeBlockMatrix{ℂ}},
     ψ::AbstractMatrix{ℂ},
@@ -207,7 +213,6 @@ function _batched_mat_el!(
     A = ws.A
     temp = ws.temp
     A .= ψ .* conj.(transpose(view(ψ, b, :)))
-    # @info "" A G
     _mul_with_strides!(temp, A, G, one(ℂ), zero(ℂ))
     dot_batched!(out, A, temp)
     out
@@ -267,6 +272,64 @@ function dielectric(χ::AbstractMatrix{Complex{ℝ}}, V::AbstractMatrix{ℝ}) wh
 end
 
 
+function _momentum_eigenvectors(
+    q::NTuple{3, ℝ},
+    x::AbstractVector{ℝ},
+    y::AbstractVector{ℝ},
+    z::AbstractVector{ℝ};
+    n::Int,
+) where {ℝ <: Real}
+    if n < 1
+        throw(ArgumentError("invalid number of points 'n': $n; expected a positive number"))
+    end
+    ks = 0:(π / (n - 1)):π
+    @assert length(ks) == n
+    out = similar(x, complex(eltype(x)), length(x), length(ks))
+    @inbounds for i in 1:size(out, 2)
+        fn = let kˣ = q[1] * ks[i], kʸ = q[2] * ks[i], kᶻ = q[3] * ks[i]
+            (xᵢ, yᵢ, zᵢ) -> exp(1im * (kˣ * xᵢ + kʸ * yᵢ + kᶻ * zᵢ))
+        end
+        map!(fn, view(out, :, i), x, y, z)
+    end
+    out
+end
+function _dispersion_function(
+    q::NTuple{3, ℝ},
+    x::AbstractVector{ℝ},
+    y::AbstractVector{ℝ},
+    z::AbstractVector{ℝ};
+    n::Int,
+) where {ℝ <: Real}
+    if length(x) != length(y) || length(x) != length(z)
+        throw(DimensionMismatch("'x', 'y', and 'z' have different lengths: $(length(x)) vs. $(length(y)) vs. $(length(z))"))
+    end
+    return let ks = _momentum_eigenvectors(q, x, y, z; n = n),
+        temp = similar(ks, length(x), size(ks, 2))
+
+        (out, A) -> begin
+            mul!(temp, A, ks)
+            @inbounds for i in 1:length(out)
+                out[i] = dot(view(ks, :, i), view(temp, :, i))
+            end
+        end
+    end
+end
+"""
+    dispersion(A(ω, r, r'), q, x, y, z; n = 100) -> A(ω, q)
+
+Calculate diagonal elements of the Fourier transform of matrix A.
+"""
+function dispersion(As, q, x, y, z; n::Int = 100)
+    fn! = _dispersion_function(q, x, y, z; n = n)
+    out = similar(x, complex(eltype(x)), length(εs), n)
+    for (i, A) in enumerate(As)
+        fn!(view(out, i, :), A)
+    end
+    out
+end
+
+
+
 function main(
     E::Vector{ℝ},
     ψ::Matrix{ℂ};
@@ -282,19 +345,6 @@ function main(
     end
     if η <= 0
         throw(ArgumentError("invalid 'η': $η; expected a positive real number"))
-    end
-    if isnothing(E) || isnothing(ψ)
-        if isnothing(H)
-            throw(ArgumentError(
-                "if Hamiltonian 'H' is not specified, both eigenvalues 'E'" *
-                " and eigenvectors 'ψ' must be provided",
-            ))
-        end
-        @info "Eigenvalues or eigenvectors not provided: diagonalizing the Hamiltonian"
-        factorization = eigen(Hermitian(H))
-        E = factorization.values
-        ψ = factorization.vectors
-        @info "Diagonalization completed"
     end
     HDF5.attributes(out)["kT"] = kT
     HDF5.attributes(out)["μ"] = μ
@@ -323,7 +373,7 @@ function main(
     end
 end
 function main(H::Matrix{ℂ}; kwargs...) where {ℂ}
-    @info "Eigenvalues or eigenvectors not provided: diagonalizing the Hamiltonian"
+    @warn "Eigenvalues or eigenvectors not provided: diagonalizing the Hamiltonian..."
     factorization = eigen(Hermitian(H))
     E = factorization.values
     ψ = factorization.vectors
@@ -405,10 +455,9 @@ function julia_main()::Cint
         tryread(io, get(args, :coulomb, nothing))
     end
     if (isnothing(E) || isnothing(ψ)) && isnothing(H)
-        throw(ArgumentError(
-            "if Hamiltonian 'H' is not specified, both eigenvalues 'E'" *
-            " and eigenvectors 'ψ' must be provided",
-        ))
+        @error "if Hamiltonian 'H' is not specified, both eigenvalues 'E'" *
+               " and eigenvectors 'ψ' must be provided"
+        return 1
     end
     h5open(args[:output_file], "w") do io
         kwargs = (

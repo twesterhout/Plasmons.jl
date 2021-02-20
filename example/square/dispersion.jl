99f38df0df404a24d5d2db96bcfec0af7e4bf77e
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-02-20 16:15:21 +0100

    Make data_for_figure take system width as argument

--- example/square/dispersion.jl
+++ example/square/dispersion.jl
@@ -12,21 +12,21 @@ theme(
     fglegend = RGB(7 / 255, 54 / 255, 66 / 255),
 )
 
-function data_for_figure_1()
-    x, y, z = h5open("input_square_sheet_32x32.h5", "r") do io
+function data_for_figure_1(n::Int = 32)
+    x, y, z = h5open("input_square_sheet_$(n)x$(n).h5", "r") do io
         read(io["x"]), read(io["y"]), read(io["z"])
     end
-    χ, ε, ωs, qs = h5open("result_square_sheet_32x32.h5", "r") do io
+    χ, ε, ωs, qs = h5open("result_square_sheet_$(n)x$(n).h5", "r") do io
         ωs = map(d->real(read(attributes(d)["ħω"])), io["/ε"])
-        n = 32 + 1
-        qs = collect(0:(π / (n - 1)):π)
+        number_qs = n + 1
+        qs = collect(0:(π / (number_qs - 1)):π)
         direction = (1.0, 0.0, 0.0)
         # Each row corresponds to an ħω, each column -- to a q
         χ = dispersion((read(d) for d in io["/χ"]), map(q-> q .* direction, qs), x, y, z)
         ε = dispersion((read(d) for d in io["/ε"]), map(q-> q .* direction, qs), x, y, z)
         χ, ε, ωs, qs
     end
-    h5open("dispersion_square_sheet_32x32.h5", "w") do io
+    h5open("dispersion_square_sheet_$(n)x$(n).h5", "w") do io
         io["ε"] = ε
         io["χ"] = χ
         io["ħω"] = ωs

46da570bbaab4231046868862cfc8d7a0146a0c8
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-02-20 14:43:17 +0100

    Plotting

--- example/square/dispersion.jl
+++ example/square/dispersion.jl
@@ -1,24 +1,48 @@
 using HDF5
+using LaTeXStrings
 using Plasmons
+using Plots
 
-load_coordinates(file::HDF5.File) = read(file["x"]), read(file["y"]), read(file["z"])
-load_coordinates(file::AbstractString) = h5open(load_coordinates, file, "r")
+pgfplotsx()
+theme(
+    :solarized_light;
+    fg = RGB(88 / 255, 110 / 255, 117 / 255),
+    fgtext = RGB(7 / 255, 54 / 255, 66 / 255),
+    fgguide = RGB(7 / 255, 54 / 255, 66 / 255),
+    fglegend = RGB(7 / 255, 54 / 255, 66 / 255),
+)
 
-matrix_iterator(file::HDF5.File, group::AbstractString) = (read(d) for d in file[group])
-function matrix_iterator(file::AbstractString, group::AbstractString)
-    io = h5open(file, "r")
-    matrix_iterator(io, group)
+function data_for_figure_1()
+    x, y, z = h5open("input_square_sheet_32x32.h5", "r") do io
+        read(io["x"]), read(io["y"]), read(io["z"])
+    end
+    matrix, ωs, qs = h5open("result_square_sheet_32x32.h5", "r") do io
+        εs = (read(d) for d in io["/ε"])
+        ωs = map(d->real(read(attributes(d)["ħω"])), io["/ε"])
+        n = 32 + 1
+        qs = collect(0:(π / (n - 1)):π)
+        direction = (1.0, 0.0, 0.0)
+        # Each row corresponds to an ħω, each column -- to a q
+        matrix = dispersion(εs, map(q-> q .* direction, qs), x, y, z)
+        matrix, ωs, qs
+    end
+    h5open("dispersion_square_sheet_32x32.h5", "w") do io
+        io["data"] = matrix
+        io["ħω"] = ωs
+        io["q"] = qs
+    end
+    nothing
 end
 
 function figure_1()
-    x, y, z = load_coordinates("input_square_sheet_32x32.h5")
-    εs = matrix_iterator("result_square_sheet_32x32.h5", "/ε")
-    q = (1.0, 0.0, 0.0)
-
-    m = dispersion(εs, q, x, y, z; n = 100)
-
-
-    h5open("dispersion_square_sheet.h5", "w") do io
-        io["m"] = m
+    matrix, ωs, qs = h5open("dispersion_square_sheet_32x32.h5", "r") do io
+        read(io["data"]), read(io["ħω"]), read(io["q"])
     end
+    # Compute loss function
+    loss = @. -imag(1 / matrix)
+    loss = transpose(loss)
+    g = heatmap(qs, ωs, loss, ylims = (0, 15), xlabel = "q, 1/a", ylabel = "ħω, eV",
+                title = "-Im[1 / ε(q, ω)]")
+    savefig(g, "dispersion_square_sheet_32x32.png")
+    nothing
 end
--- example/square/run.sh
+++ example/square/run.sh
@@ -1,26 +1,31 @@
-#!/bin/sh
+#!/usr/bin/env bash
 trap '{ rm "$input_file"; }' EXIT
 set -e
+set -o pipefail
 
-export OMP_NUM_THREADS=4
+export OMP_NUM_THREADS=16
 a=2.46                        # Lattice constant in Å
 t=2.7                         # Hopping parameter in eV
-n=5                           # System size
+n=32                          # System size
 kT=0.0256                     # Temperature in eV
-mu=3.0                        # Chemical potential μ in eV
+mu=1.0                        # Chemical potential μ in eV
 damping=0.1                   # Damping η in eV
-freq=$(seq -s, 0.0 0.05 12.0) # Comma-separated list of frequencies ω in eV
+freq=$(seq -s, 0.0 0.1 20.0)  # Comma-separated list of frequencies ω in eV
 input_file="temp_square_sheet_${n}x${n}.h5"
 
 # Generate input using TiPSi
+. $(dirname "$CONDA_EXE")/../etc/profile.d/conda.sh
+conda activate tipsi_devel
 python3 build.py -n "$n" -t "$t" -a "$a" "$input_file"
 
 # Compute χ using Plasmons.jl
 julia -e 'import Plasmons; Plasmons.julia_main()' -- \
 	--kT "$kT" --mu "$mu" --damping "$damping" \
 	--hamiltonian H \
+	--coulomb V \
 	--frequency "$freq" \
-	"$input_file" "square_sheet_${n}x${n}_new.h5"
+	--cuda 0 \
+	"$input_file" "result_square_sheet_${n}x${n}.h5"
 # TMPDIR=$HOME/tmp singularity run --fakeroot --bind=$PWD ../../Plasmons.sif \
 # 	--kT "$kT" --mu "$mu" --damping "$damping" \
 # 	--hamiltonian H \

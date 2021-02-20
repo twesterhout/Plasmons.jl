953167fd7a8070ee52c99750da0c5f46b6426c5a
Tom Westerhout <14264576+twesterhout@users.noreply.github.com>
2021-02-20 16:11:51 +0100

    Store both eps and chi in dispersion hdf5 files

--- example/square/dispersion.jl
+++ example/square/dispersion.jl
@@ -16,18 +16,19 @@ function data_for_figure_1()
     x, y, z = h5open("input_square_sheet_32x32.h5", "r") do io
         read(io["x"]), read(io["y"]), read(io["z"])
     end
-    matrix, ωs, qs = h5open("result_square_sheet_32x32.h5", "r") do io
-        εs = (read(d) for d in io["/ε"])
+    χ, ε, ωs, qs = h5open("result_square_sheet_32x32.h5", "r") do io
         ωs = map(d->real(read(attributes(d)["ħω"])), io["/ε"])
         n = 32 + 1
         qs = collect(0:(π / (n - 1)):π)
         direction = (1.0, 0.0, 0.0)
         # Each row corresponds to an ħω, each column -- to a q
-        matrix = dispersion(εs, map(q-> q .* direction, qs), x, y, z)
-        matrix, ωs, qs
+        χ = dispersion((read(d) for d in io["/χ"]), map(q-> q .* direction, qs), x, y, z)
+        ε = dispersion((read(d) for d in io["/ε"]), map(q-> q .* direction, qs), x, y, z)
+        χ, ε, ωs, qs
     end
     h5open("dispersion_square_sheet_32x32.h5", "w") do io
-        io["data"] = matrix
+        io["ε"] = ε
+        io["χ"] = χ
         io["ħω"] = ωs
         io["q"] = qs
     end
